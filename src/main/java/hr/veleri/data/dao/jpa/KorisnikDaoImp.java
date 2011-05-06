d9384b56ac3943e62c424de5b50fb1e89ee61434
ivanovicmeister <ivanovicmeister@gmail.com>
2011-05-06 12:39:46 +0000

    css for userinfo box, defining user type at logon time

--- src/main/java/hr/veleri/data/dao/jpa/KorisnikDaoImp.java
+++ src/main/java/hr/veleri/data/dao/jpa/KorisnikDaoImp.java
@@ -2,7 +2,12 @@ package hr.veleri.data.dao.jpa;
 
 import hr.veleri.AuthenticationException;
 import hr.veleri.data.dao.interfaces.KorisnikDao;
+import hr.veleri.data.dao.interfaces.KorisnikKlijentDao;
+import hr.veleri.data.dao.interfaces.KorisnikZaposlenikDao;
 import hr.veleri.data.dataobjects.Korisnik;
+import hr.veleri.data.dataobjects.KorisnikKlijent;
+import hr.veleri.data.dataobjects.KorisnikZaposlenik;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.springframework.orm.jpa.JpaCallback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +17,12 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class KorisnikDaoImp extends AbstractDaoJPAImpl<Korisnik> implements KorisnikDao {
+
+    @SpringBean
+    private KorisnikKlijentDao korisnikKlijentDao;
+
+    @SpringBean
+    private KorisnikZaposlenikDao korisnikZaposlenikDao;
 
     public KorisnikDaoImp() {
         super(Korisnik.class);
@@ -39,14 +50,40 @@ public class KorisnikDaoImp extends AbstractDaoJPAImpl<Korisnik> implements Kori
 
     }
 
+    @Transactional
+    public boolean isKorisnikKlijent(Korisnik korisnik) {
+        for (KorisnikKlijent korisnikKlijent : korisnikKlijentDao.findAll()) {
+            if (korisnikKlijent.getKorisnik().equals(korisnik))
+                return true;
+        }
+        return false;
+    }
+
+    @Transactional
+    public boolean isKorisnikZaposlenik(Korisnik korisnik) {
+        for (KorisnikZaposlenik korisnikZaposlenik : korisnikZaposlenikDao.findAll()) {
+            if (korisnikZaposlenik.getKorisnik().equals(korisnik))
+                return true;
+        }
+        return false;
+    }
+
     public Korisnik getKorisnik(final String username, final String password) {
         List<Korisnik> list = findAll();
         // administracija: admin - the MD5 encoder
-        list.add(new Korisnik("Administrator","sustava","admin","9031d52735e2a26a00a7d0d1c94d4743"));
+        list.add(new Korisnik("Administrator", "sustava", "admin", "9031d52735e2a26a00a7d0d1c94d4743"));
         for (Korisnik korisnik : list) {
-            if (korisnik.authenticate(username,password))
+            if (korisnik.authenticate(username, password))
                 return korisnik;
         }
         throw new AuthenticationException();
+    }
+
+    public void setKorisnikKlijentDao(KorisnikKlijentDao korisnikKlijentDao) {
+        this.korisnikKlijentDao = korisnikKlijentDao;
+    }
+
+    public void setKorisnikZaposlenikDao(KorisnikZaposlenikDao korisnikZaposlenikDao) {
+        this.korisnikZaposlenikDao = korisnikZaposlenikDao;
     }
 }
